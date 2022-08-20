ec714f6f50de2716ba6ed32e26c290edf78b1c2d
soochan <tncksdl05@gmail.com>
2022-08-20 15:01:53 +0900

    refactor : change entity of member
    username 추가, 단일 권한을 갖도록 변경
    security config에서 특정 url에 다중 권한을 열어놓기 위해서

--- src/main/java/server/jwt/redis/domain/Member.java
+++ src/main/java/server/jwt/redis/domain/Member.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import server.jwt.redis.domain.enums.Role;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,27 +18,30 @@ public class Member {
     private Long id;
 
     @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String nickname;
 
-    @Column(nullable = false)
-    private String password;
-
     @Enumerated(EnumType.STRING)
     private Role role;
 
 
-    private Member(String email, String nickname, String password) {
+    private Member(String username, String password, String email , String nickname , Role role) {
+        this.username = username;
+        this.password = password;
         this.email = email;
         this.nickname = nickname;
-        this.password = password;
-        this.role = Role.ROLE_USER; // hasRole("ROLE_USER")
+        this.role = role;
     }
 
-    public static Member of(String email, String nickname, String password) {
-        return new Member(email, nickname, password);
+    public static Member createMember(String username, String password, String email, String nickname, Role role) {
+        return new Member(username, password, email, nickname, role);
     }
 
 }
